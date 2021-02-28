dc0f70ecbfe89a68541554fd66ec817565d9886d
Jayesh Bhardwaj <jayeshb@amazon.com>
2021-02-28 23:53:03 +0530

    Add utils

--- src/koans/ask_koans.clj
+++ src/koans/ask_koans.clj
@@ -2,27 +2,9 @@
   (:gen-class
    :name koans.askLambda
    :methods [[handleLambda [java.util.Map] java.util.Map]])
-  (:use [koans.koan-dao]))
+  (:use [koans.koan-dao]
+        [koans.utils]))
 
-
-(defn create-simple-response
-  "Creates a skill response with the given title and text. Optionally closes session.
-  The response specifies output speech and card."
-  [text shouldEndSession]
-  {"version"           "1.0"
-   "sessionAttributes" {}
-   "response"          {"outputSpeech"     {"type" "PlainText"
-                                            "text" text}
-
-                        "shouldEndSession" shouldEndSession}})
-
-
-(def help-text "Say tell me a zen koan to get started.")
-
-(defn extract-intent-name
-  "Extracts intent name from the given skill request structure."
-  [request & not-found]
-  (get-in request ["request" "intent" "name"] not-found))
 
 (defmulti handle-intent
   (fn [req] (-> req extract-intent-name keyword)))
